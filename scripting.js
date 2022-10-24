52b7a9aec1268b9bd867aa9b89338ed86b30f6de
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-10-24 10:07:27 -0700

    disabled create-account button upon non-matching passwords and added non-match error message

--- scripting.js
+++ scripting.js
@@ -16,6 +16,8 @@ tel_input.addEventListener('keypress',(event) => {
     }
 });
 
+const submit = document.querySelector('button.create-account');
+const password_message = document.querySelector('.passwords-do-not-match');
 
 // check that passwords match
 
@@ -26,8 +28,12 @@ confirm_password.addEventListener('keyup', event => {
     if (password.value==confirm_password.value) {
         confirm_password.style.bordercolor = 'rgb(0,255,0)';
         confirm_password.style.outlineColor = 'rgb(0,255,0)';
+        submit.disabled=false;
+        password_message.style.visibility = 'hidden';
     } else {
         confirm_password.style.bordercolor = 'auto';
         confirm_password.style.outlineColor = 'white';
+        submit.disabled=true;
+        password_message.style.visibility = 'visible';
     }
 });
