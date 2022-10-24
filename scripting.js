59a4eef660f71bb24142cf3644536548fa9e05d6
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-10-24 08:29:24 -0700

    disabled non-number typing into telephone input

--- scripting.js
+++ scripting.js
@@ -1,6 +1,17 @@
 const tel_input = document.querySelector('input#phone');
+
+// auto add hyphens to phone-number
 tel_input.addEventListener('keyup',(event) => {
     if (event.key != 'Backspace' && (tel_input.value.length==3 || tel_input.value.length==7)) {
-        tel_input.value+='-';
+        if (tel_input.value[tel_input.value.length-1] != '-') {
+            tel_input.value+='-';
+        }
+    }
+})
+
+// prevent user from adding hyphens
+tel_input.addEventListener('keypress',(event) => {
+    if (!'1234567890'.includes(event.key)) {
+        event.preventDefault();
     }
 })
